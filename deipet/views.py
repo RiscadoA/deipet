e0babdef3298dc07eecad1f40954e86427cba61f
Ricardo Antunes <me@riscadoa.com>
2021-04-06 13:36:43 +0100

    Added navigation buttons to pet list

--- deipet/views.py
+++ deipet/views.py
@@ -2,15 +2,40 @@ import requests
 from django.shortcuts import render
 
 
+PETSTORE_PETS_END_POINT = 'https://aduck.rnl.tecnico.ulisboa.pt/deipet/pets'
+
 def index(request):
     return render(request, 'index.html', {})
 
 def pets(request):
-    response = requests.get('https://aduck.rnl.tecnico.ulisboa.pt/deipet/pets')
+    
+    if 'limit' in request.GET:
+        limit = int(request.GET['limit'])
+    else:
+        limit = 10
+
+    if 'offset' in request.GET:
+        offset = int(request.GET['offset'])
+    else:
+        offset = 0
+
+    response = requests.get(PETSTORE_PETS_END_POINT, params={
+        "limit": limit,
+        "offset": offset
+    })
+
     pets = response.json()
-    return render(request, 'pets.html', {"pets": pets})
+    return render(request, 'pets.html', {
+        "limit": limit,
+        "offset": offset,
+        "prevOffset": max(0, offset - limit),
+        "nextOffset": pets[-1]["id"] if len(pets) == limit else offset,
+        "first": offset + 1,
+        "last": min(len(pets), limit) + offset,
+        "pets": pets
+    })
 
 def pet(request, id):
-    response = requests.get('https://aduck.rnl.tecnico.ulisboa.pt/deipet/pets/%d' % id)
+    response = requests.get('%s/%d' % (PETSTORE_PETS_END_POINT, id))
     pet = response.json()
     return render(request, 'pet.html', {"pet": pet})
